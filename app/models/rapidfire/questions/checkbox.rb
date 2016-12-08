9df086fc71fb791b7bf4c156626c7707c583b416
Raphael Ferreira <raphaelbsferreira@gmail.com>
2016-12-08 19:03:07 -0200

    Changes for making questions being created with the survey

--- app/models/rapidfire/questions/checkbox.rb
+++ app/models/rapidfire/questions/checkbox.rb
@@ -11,6 +11,7 @@ module Rapidfire
         super(answer)
 
         if rules[:presence] == "1" || answer.answer_text.present?
+          byebug
           answer.answer_text.split(Rapidfire.answers_delimiter).each do |value|
             answer.errors.add(:answer_text, :invalid) unless options.include?(value)
           end
