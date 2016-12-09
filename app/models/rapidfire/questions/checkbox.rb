785d06262c7ab0ed8cfd1a92b2d909fc4a0800f0
Raphael Ferreira <raphaelbsferreira@gmail.com>
2016-12-09 11:50:58 -0200

    removed byebug

--- app/models/rapidfire/questions/checkbox.rb
+++ app/models/rapidfire/questions/checkbox.rb
@@ -11,7 +11,6 @@ module Rapidfire
         super(answer)
 
         if rules[:presence] == "1" || answer.answer_text.present?
-          byebug
           answer.answer_text.split(Rapidfire.answers_delimiter).each do |value|
             answer.errors.add(:answer_text, :invalid) unless options.include?(value)
           end
