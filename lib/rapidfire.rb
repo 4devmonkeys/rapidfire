d098dd68f8261e8b95f3a6c5c1d6b552212309c8
Raphael Ferreira <raphaelbsferreira@gmail.com>
2017-01-07 18:37:50 -0200

    added a new error, AnswerDenied

--- lib/rapidfire.rb
+++ lib/rapidfire.rb
@@ -1,9 +1,11 @@
 require "rapidfire/engine"
 
 module Rapidfire
-  class AccessDenied < StandardError
-  end
+    class AccessDenied < StandardError
+    end
 
+    class AnswerDenied < StandardError
+    end
   # configuration which will be used as delimiter in case answers are bunch
   # of collection values. This is the default delimiter which is used by
   # all the browsers.
